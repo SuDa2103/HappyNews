fa545c9c32771f579339a0d42dfd8b5ecee3d17a
Sunny Dasgupta <SunnyDasgupta11@gmail.com>
2020-10-24 21:22:28 +0100

    This is stressing me out

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,5 +1,5 @@
 class LinksController < ApplicationController
-  skip_before_action :authenticate_user!, only: :index
+  skip_before_action :authenticate_user!, only: [:index, :show]
   def index
     @links = Link.all
   end
