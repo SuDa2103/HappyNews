8d845c356c7ead77db6cf54587fc6b3570461b28
Sunny Dasgupta <SunnyDasgupta11@gmail.com>
2020-10-24 20:17:32 +0100

    Devise with username added to the user model

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,8 @@ ActiveRecord::Schema.define(version: 2020_10_24_185906) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
+    t.string "email", default: ""
+    t.string "username", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
