c04d9fbcde274d3092e51906152146bffe374d02
Sunny Dasgupta <SunnyDasgupta11@gmail.com>
2020-10-26 20:16:10 +0000

    Navbar changes

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,5 +1,5 @@
 class LinksController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index, :show]
+  skip_before_action :authenticate_user!, only: [:index, :show, :newest]
   def index
     @links = Link.hottest
   end
